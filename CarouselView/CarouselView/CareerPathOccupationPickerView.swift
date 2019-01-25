d5c87112cd786605387b7ecad66ff09bfcd11a82
aDreamCatcher <344944410@qq.com>
2019-01-25 13:42:05 +0800

    annotation

--- CarouselView/CarouselView/CareerPathOccupationPickerView.swift
+++ CarouselView/CarouselView/CareerPathOccupationPickerView.swift
@@ -157,12 +157,11 @@ class CareerPathOccupationPickerView: UIView {
         let scrollViewWidth = scrollView.frame.size.width
         let centerX = scrollView.contentOffset.x + scrollViewWidth * 0.5
         
-        
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         let cellWidth = layout.itemSize.width
         let cellOccupiedWidth = cellWidth + Constant.designItemSpace
         let leftEdge = (scrollViewWidth - cellWidth) * 0.5
-        
+        // get current item's index
         for index in 0..<dataSource.count {
             let cellOriginX = leftEdge + CGFloat(index) * cellOccupiedWidth
             let cellEndX = cellOriginX + cellWidth
