a09e352ce89af27043bd1c417fc85d06d07365e1
aDreamCatcher <344944410@qq.com>
2019-02-27 18:33:15 +0800

    UIDocumentInteractionViewController

--- ExampleShareActivity/ExampleShareActivity/ViewController.swift
+++ ExampleShareActivity/ExampleShareActivity/ViewController.swift
@@ -10,33 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var documentInteractionController: UIDocumentInteractionController? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         let button = UIButton(type: .custom)
-        button.setTitle("PresentShare", for: .normal)
+        button.setTitle("activityShare", for: .normal)
         button.setTitleColor(UIColor.red, for: .normal)
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         button.frame = CGRect(x: 20, y: 100, width: 200, height: 100)
 
-        view.addSubview(button)
+        let button2 = UIButton(type: .custom)
+        button2.setTitle("documentShare", for: .normal)
+        button2.setTitleColor(UIColor.red, for: .normal)
+        button2.addTarget(self, action: #selector(buttonAction2), for: .touchUpInside)
+        button2.frame = CGRect(x: 20, y: 220, width: 200, height: 100)
 
-//        // UIImageView
-//
-//        let imgV = UIImageView(image: UIImage(named: "face"))
-//        imgV.frame = CGRect(x: 20, y: 220, width: 200, height: 200)
-//        view.addSubview(imgV)
+        view.addSubview(button)
+        view.addSubview(button2)
     }
+    
+}
+
+// MARK: UIActivityViewController
+extension ViewController {
 
     @objc public func buttonAction() {
-
         // UIActivityItems
         let textItem = ExampleActivityItem("Pig")
         let imageItem = ExampleActivityItem("Smiling Face", image: UIImage(named: "face"))
         let customItem = ExampleActivityItem("multi-data", image: UIImage(named: "face"), subject: "This is a smile.")
 
-        let activityItems = [textItem, imageItem ,customItem]
+        let activityItems = [textItem, imageItem, customItem]
 
         // UIActivitys
         let activity = ExampleActivity("SmilingFace", image: UIImage(named: "face_60")) { (items) in
@@ -57,7 +64,60 @@ class ViewController: UIViewController {
 
         present(activityViewController, animated: true, completion: nil)
     }
+}
 
+// MARK: UIDocumentInteractionController
+
+extension ViewController {
+
+    @objc public func buttonAction2() {
+        //        let facePath = Bundle.main.path(forResource: "face", ofType: "png")
+        //        let faceURL = URL(fileURLWithPath: facePath ?? "")
+
+        let filePath = textFilePath("344944410@qq.com")
+        let fileURL = URL(fileURLWithPath: filePath)
+
+        documentInteractionController = UIDocumentInteractionController(url: fileURL)
+        documentInteractionController?.delegate = self
+
+        //        documentInteractionController?.presentPreview(animated: true)
+        documentInteractionController?.presentOptionsMenu(from: .zero,
+                                                          in: view,
+                                                          animated: true)
+    }
+
+    private func textFilePath(_ content: String) -> String {
+        let dirPath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let filePath = dirPath.appending("/local.txt")
+
+        let fileExist = FileManager.default.fileExists(atPath: filePath)
+        if !fileExist {
+        }
+
+        do {
+            try content.write(toFile: filePath, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("")
+        }
+
+
+        return filePath
+    }
+}
+
+extension ViewController: UIDocumentInteractionControllerDelegate {
+
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        return self
+    }
+
+    func documentInteractionControllerViewForPreview(_ controller: UIDocumentInteractionController) -> UIView? {
+        return view
+    }
+
+    func documentInteractionControllerRectForPreview(_ controller: UIDocumentInteractionController) -> CGRect {
+        return view.bounds
+    }
 
 }
 
