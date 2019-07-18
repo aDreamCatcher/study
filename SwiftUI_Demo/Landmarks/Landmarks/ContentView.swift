1b34d7200aefeb83d1e12cbb62923cfccd424eb5
aDreamCatcher <344944410@qq.com>
2019-07-18 18:26:46 +0800

    SwiftUI Essentials: Creating and Combining Views

---
+++ SwiftUI_Demo/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Guiyang Li on 2019/7/18.
+//  Copyright © 2019 Xin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Hello World")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+
+            Spacer()
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
